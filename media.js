8de81539f06dfa34f2bd54b4a89377937a694bfa
Patricia Reghine <patricia.schimanski@escola.pr.gov.br>
2026-01-21 22:55:14 -0300

    pegando a lista de numeros e realizando a soma

--- media.js
+++ media.js
@@ -1,8 +1,17 @@
 let numeros =  [];
 
-let entrada = prompt("Insira um numero");
-let numero = Number(entrada);
+let tamanhoLista = prompt("Quantos numeros voce quer inserir?")
+let soma = 0
 
-numeros.push(numero);
+for (let i = 0; i < tamanhoLista; i++) {
+    let entrada = prompt("Insira um numero");
+    let numero = Number(entrada);
+    numeros.push(numero);
+}
+
+for (let i = 0; i < numeros.length; i++) {
+    soma += numeros[i];
+}
 
 console.log(numeros);
+console.log("A soma dos numeros é: " + soma);
