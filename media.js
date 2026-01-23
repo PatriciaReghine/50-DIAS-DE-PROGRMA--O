572c76b04d611bd272566b509eeb4785d92c1ece
Patricia Reghine <patricia.schimanski@escola.pr.gov.br>
2026-01-22 22:55:40 -0300

    realizando a média, soma, numero maior e menor

--- media.js
+++ media.js
@@ -3,6 +3,8 @@ let numeros =  [];
 let tamanhoLista = prompt("Quantos numeros voce quer inserir?")
 let soma = 0
 
+
+
 for (let i = 0; i < tamanhoLista; i++) {
     let entrada = prompt("Insira um numero");
     let numero = Number(entrada);
@@ -11,7 +13,30 @@ for (let i = 0; i < tamanhoLista; i++) {
 
 for (let i = 0; i < numeros.length; i++) {
     soma += numeros[i];
+
+
 }
+
+let maior = numeros[0]
+let menor = numeros [0]
+
+for (let i = 1; i < numeros.length; i++) {
+        if (numeros[i] > maior) {
+        maior = numeros[i];
+    } else if (numeros[i] < menor) {
+        menor = numeros[i];
+    }
+}
+
+let media = soma / numeros.length;
+
+
 
 console.log(numeros);
 console.log("A soma dos numeros é: " + soma);
+
+console.log("A média dos numeros é: " + media);
+
+console.log("O maior numero é: " + maior);
+
+console.log("O menor numero é: " + menor);
